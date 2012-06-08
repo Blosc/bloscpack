1e82b72e4cee3e588fad3852a36c37ae98517395
Valentin Haenel <valentin.haenel@gmx.de>
2012-06-08 22:45:35 +0200

    whitespace

--- bloscpack.py
+++ bloscpack.py
@@ -352,7 +352,7 @@ def calculate_nchunks(in_file_size, nchunks=None, chunk_size=None):
             nchunks = quotient + 1
             last_chunk_size = remainder
     elif nchunks == None and chunk_size == None:
-        nchunks =  int(math.ceil(in_file_size/blosc.BLOSC_MAX_BUFFERSIZE)) 
+        nchunks =  int(math.ceil(in_file_size/blosc.BLOSC_MAX_BUFFERSIZE))
         quotient, remainder = divmod(in_file_size, blosc.BLOSC_MAX_BUFFERSIZE)
         if in_file_size == blosc.BLOSC_MAX_BUFFERSIZE:
             nchunks = 1

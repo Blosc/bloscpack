1a5072f0d54a467b407cc4f4931f488e076b505a
Valentin Haenel <valentin.haenel@gmx.de>
2013-03-13 22:23:06 +0100

    remove left over debugging statement found in test-suite

--- test_bloscpack.py
+++ test_bloscpack.py
@@ -52,7 +52,6 @@ def test_serializers():
     nt.assert_equal(SERIZLIALIZERS_AVAIL, ['JSON'])
     output = '{"dtype":"float64","shape":[1024],"others":[]}'
     input_ = eval(output)
-    print(input_, type(input_))
     for s in SERIZLIALIZERS:
         nt.assert_equal(output, s.dumps(input_))
         nt.assert_equal(input_, s.loads(output))

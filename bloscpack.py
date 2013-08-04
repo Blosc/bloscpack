b41a064640fac3a74d79f7b301f2dc5c042f7395
Valentin Haenel <valentin.haenel@gmx.de>
2013-08-04 12:49:39 +0200

    bump version for next release

--- bloscpack.py
+++ bloscpack.py
@@ -22,7 +22,7 @@ except ImportError:
     from ordereddict import OrderedDict
 import blosc
 
-__version__ = '0.3.0'
+__version__ = '0.4.0-dev'
 __author__ = 'Valentin Haenel <valentin.haenel@gmx.de>'
 
 # miscellaneous

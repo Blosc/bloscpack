e05c2b73a6134071a203949ff11b91570fdae0d9
Valentin Haenel <valentin.haenel@gmx.de>
2013-08-18 22:00:10 +0200

    fix license, update author and long description

--- setup.py
+++ setup.py
@@ -5,20 +5,24 @@
 from distutils.core import setup
 import bloscpack as bp
 
+with open('README.rst') as fp:
+    long_description = fp.read()
+
 setup(
     name = "bloscpack",
     version = bp.__version__,
     py_modules = ['bloscpack'],
     scripts = ['blpk'],
     author = "Valentin Haenel",
-    author_email = "valentin.haenel@gmx.de",
-    description = "Command line interface to Blosc via python-blosc",
+    author_email = "valentin@haenel.co",
+    description = "Command line interface to and serialization format for Blosc",
+    long_description=long_description,
     license = "MIT",
     keywords = ('compression', 'applied information theory'),
     url = "https://github.com/esc/bloscpack",
     classifiers = ['Development Status :: 3 - Alpha',
                    'Environment :: Console',
-                   'License :: OSI Approved ::  MIT',
+                   'License :: OSI Approved :: MIT License',
                    'Operating System :: POSIX',
                    'Programming Language :: Python',
                    'Topic :: Scientific/Engineering',

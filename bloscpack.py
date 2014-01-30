408ef3281a6565effc291ef4556c5515a097eadc
Valentin Haenel <valentin.haenel@gmx.de>
2014-01-30 20:23:05 +0100

    bump version number after release

--- bloscpack.py
+++ bloscpack.py
@@ -28,7 +28,7 @@ except ImportError:  # pragma: no cover
 import blosc
 import numpy as np
 
-__version__ = '0.5.0-rc1'
+__version__ = '0.5.0-rc2-dev'
 __author__ = 'Valentin Haenel <valentin.haenel@gmx.de>'
 
 # miscellaneous

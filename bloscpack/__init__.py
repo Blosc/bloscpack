cbd2c69af63dcac580459109f57ad2191f85b643
Valentin Haenel <valentin@haenel.co>
2014-02-15 14:41:48 +0100

    create module syutils with some small utilities

--- bloscpack/__init__.py
+++ bloscpack/__init__.py
@@ -139,17 +139,6 @@ class NotANumpyArray(RuntimeError):
     pass
 
 
-def drop_caches():  # pragma: no cover
-    if os.geteuid() == 0:
-        os.system('echo 3 > /proc/sys/vm/drop_caches')
-    else:
-        raise RuntimeError('Need root permission to drop caches')
-
-
-def sync():
-    os.system('sync')
-
-
 def decode_uint8(byte):
     return struct.unpack('<B', byte)[0]
 

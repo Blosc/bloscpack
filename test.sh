88fb70ecb14eaf7f984f7275a125e3002958c971
Valentin Haenel <valentin.haenel@gmx.de>
2014-03-13 22:17:54 +0100

    better test.sh, exit with proper exit code

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 echo "Testing command line interface with cram"
-COVERAGE=1 cram test_bloscpack.cram
+COVERAGE=1 cram --verbose test_bloscpack.cram
+cram_exit=$?
 echo "Executing unit tests with nosetests"
 nosetests --with-coverage --cover-package=bloscpack test
+nose_exit=$?
+if [ $cram_exit -gt 0 ] || [ $nose_exit -gt 0 ] ; then
+    echo "some tests failed"
+    exit 1
+fi
+

777e71f3d46db05daa5b0ad2ff464e695db8fa7c
Valentin Haenel <valentin.haenel@gmx.de>
2014-04-02 22:41:09 +0200

    refactor test file creation to avoid duplication

--- test_cmdline/mktestarray.py
+++ test_cmdline/mktestarray.py
@@ -12,12 +12,20 @@ Example usage from cram file:
 
 
 import json
+import os.path as path
 
 from bloscpack.numpy_io import _ndarray_meta
 import numpy
 
-a = numpy.linspace(0, 100, 2e7)
-with open('data.dat', 'w') as f:
-    f.write(a.tostring())
-with open('meta.json', 'w') as m:
-    m.write(json.dumps(_ndarray_meta(a)))
+DATA_FILE = 'data.dat'
+META_FILE = 'meta.json'
+
+def exists(filename):
+    return path.isfile(filename)
+
+if not exists(DATA_FILE) and not exists(META_FILE):
+    a = numpy.linspace(0, 100, 2e7)
+    with open(DATA_FILE, 'w') as f:
+        f.write(a.tostring())
+    with open(META_FILE, 'w') as m:
+        m.write(json.dumps(_ndarray_meta(a)))

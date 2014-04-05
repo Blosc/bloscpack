2446e02216bcdfe22b7b04dd18f6ede3a9652dcc
Valentin Haenel <valentin.haenel@gmx.de>
2014-04-05 20:37:37 +0200

    drop caches and sync

--- bench/compression_time_vs_chunk_size.py
+++ bench/compression_time_vs_chunk_size.py
@@ -3,6 +3,7 @@
 
 
 from __future__ import division
+from __future__ import print_function
 
 
 import os.path as path
@@ -13,6 +14,7 @@ import numpy
 
 
 import bloscpack.testutil as bpt
+from bloscpack.sysutil import drop_caches, sync
 from bloscpack.args import DEFAULT_BLOSC_ARGS
 from bloscpack import pack_file, unpack_file
 from bloscpack.pretty import pretty_size
@@ -20,18 +22,30 @@ from bloscpack.pretty import pretty_size
 blosc_args = DEFAULT_BLOSC_ARGS
 
 with bpt.create_tmp_files() as (tdir, in_file, out_file, dcmp_file):
-    bpt.create_array(100, in_file)
+
+    print('create the test data', end='')
+
+    def progress(i):
+        if i % 10 == 0:
+            print('.', end='')
+        sys.stdout.flush()
+
+    bpt.create_array(100, in_file, progress=progress)
     repeats = 3
-    print ("%s\t\t%s\t\t%s\t\t%s" %
+    print ("%s\t%s\t\t%s\t\t%s" %
            ("chunk_size", "comp-time", "decomp-time", "ratio"))
     for chunk_size in (int(2**i) for i in numpy.arange(19, 23.5, 0.5)):
         cmp_times, dcmp_times = [], []
         for _ in range(repeats):
+            sync()
+            drop_caches()
             tic = time.time()
             pack_file(in_file, out_file, chunk_size=chunk_size,
                       blosc_args=blosc_args)
             toc = time.time()
             cmp_times.append(toc-tic)
+            sync()
+            drop_caches()
             tic = time.time()
             unpack_file(out_file, dcmp_file)
             toc = time.time()

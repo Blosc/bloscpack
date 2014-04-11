22abd54eb4228138e3fe478d844b268b676aa888
Valentin Haenel <valentin.haenel@gmx.de>
2014-04-12 00:43:21 +0200

    fixup imports and indentation

--- bloscpack/__init__.py
+++ bloscpack/__init__.py
@@ -6,11 +6,13 @@
 
 from .version import __version__  # pragma: no cover
 
+from .args import (BloscArgs,
+                   )
 from .file_io import (pack_file,
-                  unpack_file,
-                  )
+                      unpack_file,
+                      )
 from .numpy_io import (pack_ndarray_file,
-                     unpack_ndarray_file,
-                     pack_ndarray_str,
-                     unpack_ndarray_str,
-                     )
+                       unpack_ndarray_file,
+                       pack_ndarray_str,
+                       unpack_ndarray_str,
+                       )

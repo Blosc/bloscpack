007b357ce711be4c600f9c897a5acd6db7d9fb92
Valentin Haenel <valentin.haenel@gmx.de>
2014-05-05 00:03:12 +0200

    don't do computationally intensive tests on drone.io for now

--- .drone.sh
+++ .drone.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt -r test_requirements.txt
 ./test.sh
-nosetests test/test_file_io.py:pack_unpack_hard
-nosetests test/test_numpy_io.py:huge_arrays
+#nosetests test/test_file_io.py:pack_unpack_hard
+#nosetests test/test_numpy_io.py:huge_arrays

fa0d9e8f793b9868d510547a387561c6311a027e
Valentin Haenel <valentin@haenel.co>
2015-05-14 23:16:34 +0200

    adding check for coverage

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if ! $( which coverage ); then
+    echo "Error: Coverage script not found!"
+    exit 1
+fi
 echo "Testing command line interface with cram"
 COVERAGE=1 cram --verbose test_cmdline/*.cram
 cram_exit=$?

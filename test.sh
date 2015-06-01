a1a43a7603329deb5f684073885464aa3f706177
Valentin Haenel <valentin@haenel.co>
2015-06-01 23:47:05 +0200

    allow ./test.sh -i to use cram in interactive mode

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ if ! $( which coverage ); then
     exit 1
 fi
 echo "Testing command line interface with cram"
-COVERAGE=1 cram --verbose test_cmdline/*.cram
+COVERAGE=1 cram --verbose $@  test_cmdline/*.cram
 cram_exit=$?
 echo "Executing unit tests with nosetests"
 nosetests --with-coverage --cover-package=bloscpack test

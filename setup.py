79e35d484bbd5bd99e2b2b828eac9282dff2ec7f
Valentin Haenel <valentin@haenel.co>
2015-12-09 22:14:23 +0100

    adding supported python versions to trove classifiers

--- setup.py
+++ setup.py
@@ -57,5 +57,10 @@ setup(
                    'Topic :: Scientific/Engineering',
                    'Topic :: System :: Archiving :: Compression',
                    'Topic :: Utilities',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
                   ],
      )

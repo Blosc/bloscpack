b05940c198525e6d25941c7d68f7a16818225b0d
Valentin Haenel <valentin@haenel.co>
2017-06-13 22:11:36 +0200

    fix some strange mixup from back in the day

--- bloscpack/defaults.py
+++ bloscpack/defaults.py
@@ -3,17 +3,17 @@
 # vim :set ft=py:
 
 # blosc args
+DEFAULT_TYPESIZE = 8
+DEFAULT_CLEVEL = 7
+DEFAULT_SHUFFLE = True
+DEFAULT_CNAME = 'blosclz'
+
+# bloscpack args
 DEFAULT_OFFSETS = True
 DEFAULT_CHECKSUM = 'adler32'
 DEFAULT_MAX_APP_CHUNKS = lambda x: 10 * x
 
 DEFAULT_CHUNK_SIZE = '1M'
-
-# bloscpack args
-DEFAULT_TYPESIZE = 8
-DEFAULT_CLEVEL = 7
-DEFAULT_SHUFFLE = True
-DEFAULT_CNAME = 'blosclz'
 
 # metadata args
 DEFAULT_MAGIC_FORMAT = b'JSON'

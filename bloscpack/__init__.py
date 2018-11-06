3d4eef3a1fd36e8d01b8f9e54b0e93affaeb33d8
Valentin Haenel <valentin@haenel.co>
2018-11-06 20:53:22 +0100

    seperate deprecated imports

--- bloscpack/__init__.py
+++ bloscpack/__init__.py
@@ -10,23 +10,27 @@ from .args import (BloscArgs,
                    BloscpackArgs,
                    MetadataArgs,
                    )
-from .file_io import (pack_file,
-                      pack_file_to_file,
-                      unpack_file,
+from .file_io import (pack_file_to_file,
                       unpack_file_from_file,
-                      pack_bytes_file,
                       pack_bytes_to_file,
-                      unpack_bytes_file,
                       unpack_bytes_from_file,
                       pack_bytes_to_bytes,
                       unpack_bytes_from_bytes,
                       )
-from .numpy_io import (pack_ndarray_file,
-                       pack_ndarray_to_file,
+# deprecated
+from .file_io import (pack_file,
+                      unpack_file,
+                      pack_bytes_file,
+                      unpack_bytes_file,
+                      )
+from .numpy_io import (pack_ndarray_to_file,
                        unpack_ndarray_from_file,
+                       pack_ndarray_to_bytes,
+                       unpack_ndarray_from_bytes,
+                       )
+# deprecated
+from .numpy_io import (pack_ndarray_file,
                        unpack_ndarray_file,
                        pack_ndarray_str,
-                       pack_ndarray_to_bytes,
                        unpack_ndarray_str,
-                       unpack_ndarray_from_bytes,
                        )

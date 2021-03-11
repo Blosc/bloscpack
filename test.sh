d17d93b84bf59658ce47e73c1daf34c435f9651a
Valentin Haenel <valentin@haenel.co>
2021-03-11 17:54:27 +0100

    Fix running pytest with coverage
    Pytest and coverage would get confused for an unknown reason presumably
    related to installed packages vs. packages in the current directory. Not
    installing bloscpack and running pytest with PYTHONPATH=. fixes this
    issue.

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ echo "Testing command line interface with cram"
 COVERAGE=1 cram --verbose $@  test_cmdline/*.cram
 cram_exit=$?
 echo "Executing unit tests with pytest"
-pytest --cov=bloscpack test
+PYTHONPATH=. pytest --cov=bloscpack test
 pytest_exit=$?
 if [ $cram_exit -gt 0 ] || [ $pytest_exit -gt 0 ] ; then
     echo "some tests failed"
